caef8134efc349e21b8eab9b721b956df68e9f07
AlexGR0 <it_zd1026265758@qq.com>
2025-03-16 14:10:08 +0800

    feat: 404页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from '@pages/NotFound.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound: React.FC = () => {
-  return <div>404 Page</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={`${styles['contain']}`}>
+      <div className={`${styles['num']}`}>404</div>
+      <div className={`${styles['text']}`}>您所访问的页面不存在</div>
+      <div
+        className={`${styles['btn']}`}
+        onClick={() => navigate('/home/index', { replace: true })}
+      >
+        返回首页
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
